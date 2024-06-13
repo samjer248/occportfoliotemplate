748d07a0b05dd70a7b2e72e4253aac6ffaff321c
Neil Mastroianni <nf_mastroianni@hotmail.com>
2024-06-13 15:01:50 -0400

    removed duplicate section

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -22,30 +22,28 @@ const Skills = () => {
     }),
   }
   return (
-    <section id="skills" ref={ref}>
-      <section
-        id="skills"
-        ref={ref}
-        className="mx-auto max-w-screen-md scroll-mt-28 text-center"
-      >
-        <SectionHeading>My skills</SectionHeading>
-        <ul className="my-6 flex flex-col justify-center gap-y-5 text-lg text-gray-800 sm:my-12 sm:mt-12 sm:flex-row sm:flex-wrap sm:gap-4">
-          {skills.map((skill, index) => (
-            <motion.li
-              key={index}
-              variants={fadeInAnimationVariants}
-              initial="initial"
-              whileInView="animate"
-              viewport={{
-                once: true,
-              }}
-              custom={index}
-            >
-              <Badge className="px-6 py-4">{skill}</Badge>
-            </motion.li>
-          ))}
-        </ul>
-      </section>
+    <section
+      id="skills"
+      ref={ref}
+      className="mx-auto max-w-screen-md scroll-mt-28 text-center"
+    >
+      <SectionHeading>My skills</SectionHeading>
+      <ul className="my-6 flex flex-col justify-center gap-y-5 text-lg text-gray-800 sm:my-12 sm:mt-12 sm:flex-row sm:flex-wrap sm:gap-4">
+        {skills.map((skill, index) => (
+          <motion.li
+            key={index}
+            variants={fadeInAnimationVariants}
+            initial="initial"
+            whileInView="animate"
+            viewport={{
+              once: true,
+            }}
+            custom={index}
+          >
+            <Badge className="px-6 py-4">{skill}</Badge>
+          </motion.li>
+        ))}
+      </ul>
     </section>
   )
 }
